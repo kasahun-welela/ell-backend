b76672abe1988f0d94d7565fa8f02d7d350bc092
kasahun welela <getasewmequanint@cbe.com.et>
2023-10-21 01:46:41 +0300

    add post to and get from db

---
+++ routes/post.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+router.post("/post", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  try {
+    const savePost = await post.save();
+    res.json(savePost);
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+});
+
+router.get("/post", async (req, res) => {
+  try {
+    const getPosts = await Post.find();
+    res.json(getPosts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
